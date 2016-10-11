fb4a684b3867c3e941ef20d5414b9b5f64c86cbc
Rehmanpa <sethexgs2@gmail.com>
2016-10-11 15:51:29 -0700

    Testing

--- src/PongPackage/SinglePlayerAIPongPanel.java
+++ src/PongPackage/SinglePlayerAIPongPanel.java
@@ -1,7 +1,14 @@
 package PongPackage;
 
+import javax.swing.*;
+
 /**
  * Created by Jonathan Kipper on 10/11/2016.
  */
-public class SinglePlayerAIPongPanel {
+public class SinglePlayerAIPongPanel extends JPanel {
+
+    static void gg() {
+        System.out.println("This is an update test");
+    }
+
 }
